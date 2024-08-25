26c7d94689592bd69940a0f637a0cd421714e4b4
Hollis Crowder <hollis.crowder@gmail.com>
2024-08-24 21:26:55 -0700

    fix login css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,14 +9,16 @@ function Login() {
     const [user, setUser, isLoggedIn] = useOutletContext();
 
     return (
-        <div className = "parent_login_container">
-            <div className = "login_container">
-                <HomescreenMessage className = "homescreen_message"/>
-                <LoginForm user={user} setUser={setUser} className = "login"/>
-                <div className = "signup_instead">
-                <br></br>
-                <p>Want to Join?</p>
-                <Link to="/">Sign Up</Link>
+        <div className = "login_screen_container">
+            <div className = "parent_login_container">
+                <div className = "login_container">
+                    <HomescreenMessage className = "homescreen_message"/>
+                    <LoginForm user={user} setUser={setUser} className = "login"/>
+                    <div className = "signup_instead">
+                    <br></br>
+                    <p>Want to Join?</p>
+                    <Link to="/">Sign Up</Link>
+                    </div>
                 </div>
             </div>
         </div>
